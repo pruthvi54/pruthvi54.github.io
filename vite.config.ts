8494119458224d16e91dfacdb6a0afa521801cf1
Pruthviraj <pruthviraj3629@gmail.com>
2025-11-01 18:18:46 +0530

    Fix base path for user site (pruthvi54.github.io)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/pruthviraj.github.io/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
