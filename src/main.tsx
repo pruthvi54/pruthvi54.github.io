dce69a99349d68c21c257adc84f0a4e5e5ea9810
Pruthviraj <pruthviraj3629@gmail.com>
2025-11-02 15:07:59 +0530

    Fix: Add .nojekyll to disable Jekyll and restore GitHub Actions build

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SimpleApp from "./SimpleApp";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SimpleApp />
+    <App />
   </React.StrictMode>
 );
